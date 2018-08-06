1c8146effdd12bfba8dc13a07c33757f5b513537
vneeli <vneeli@DIN73002715.corp.capgemini.com>
2018-08-06 12:11:00 +0530

    Added Sales Class

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,20 @@
+package org.cap.demo;
+
+public class SalesClass {
+	public static void main(String[] args) {
+	int[] arr= {1,2,3,55,-30};
+	System.out.println("Hello!!");
+     show(arr);
+	
+
+	
+		
+	}
+
+	private static void show(int[] arr) {
+		for (int i : arr) {
+			System.out.println(i);
+		}
+		
+	}
+}

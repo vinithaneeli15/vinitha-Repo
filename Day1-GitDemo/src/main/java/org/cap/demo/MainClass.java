6d89d46ecc39fb28af056a0f94ef889bc1b76cc6
vneeli <vneeli@DIN73002715.corp.capgemini.com>
2018-08-06 10:36:30 +0530

    My First Commit

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,17 @@
+package org.cap.demo;
+
+public class MainClass {
+	public static void Main(String[] args) {
+		
+		
+		System.out.println("Hello!");
+		greet();
+		
+	}
+
+	private static void greet() {
+		System.out.println("Good Morning!!");
+		
+	}
+
+}

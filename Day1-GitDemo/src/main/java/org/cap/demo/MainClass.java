4768c91237ba0ed7e991893bc35aec56ba2b2239
vneeli <vneeli@DIN73002715.corp.capgemini.com>
2018-08-06 10:52:51 +0530

    My second commit

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -1,9 +1,9 @@
 package org.cap.demo;
 
 public class MainClass {
-	public static void Main(String[] args) {
-		
-		
+
+	public static void main(String[] args) {
+
 		System.out.println("Hello!");
 		greet();
 		
@@ -11,7 +11,8 @@ public class MainClass {
 
 	private static void greet() {
 		System.out.println("Good Morning!!");
-		
+		for(int i=0;i<5;i++) {
+			System.out.println(i);
+		}
 	}
-
 }
